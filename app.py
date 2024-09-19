5af7e30f9f2401f82b203bfab70647cad2d899be
Serj <serj@serj.tf>
2024-09-19 13:41:14 +0100

    Page change

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def hello():
     cluster_name = os.environ.get('CLUSTER_NAME', 'Unknown Cluster')
     node_name = socket.gethostname()
-    return render_template('index.html', cluster_name="Openshift cluster", node_name=node_name)
+#    return render_template('index.html', cluster_name="Openshift cluster", node_name=node_name)
+    return render_template('index2.html', cluster_name="Openshift cluster", node_name=node_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
